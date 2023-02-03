c823ea4e473c5beeb15ce2ea394ba248a9ca8021
devsilva <jerson.silva99@hotmail.com>
2023-02-03 10:16:49 -0500

    8. Introducción a arrays

---
+++ Introducción a Arrays/introduccion-a-arrays.js
@@ -0,0 +1,12 @@
+// Arrays - Arreglos
+// Un array o arreglo es una lista de caracteres, sean números, string o boleans
+
+const tecnologias = [20, 30, true, 'NextJS', 'JS'] // Crear un array
+
+console.table(tecnologias) // 'object'
+
+console.log(tecnologias[3]) // Podemos inidicar el indice del array
+
+console.log(tecnologias.length) // Usando length podemos ver el tamaño del array
+
+console.log(tecnologias.toString()) // Usando toString() imprimimos todo el array en un string
