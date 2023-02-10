429ffb3110a6e33d9396c5c3003c73cd2b663a78
devsilva <jerson.silva99@hotmail.com>
2023-02-10 18:08:42 -0500

    10. Introducción destructuring de arrays

---
+++ Destructuring de arrays/destructuring-de-arrays.js
@@ -0,0 +1,6 @@
+// Destructuring de arrays
+const tecnologias = ['Esto', 'es', 'un', 'simulacro']
+
+const [, , , simulacro] = tecnologias // Puedes acceder a la posición del array colocando algún valor dentro del array, puedes omitir los otros valores y acceder al que quieres
+
+console.log(simulacro) // 'simulacro'
