11f63b50b2c381fd9cf8b5956fd53465fcbcf1fb
devsilva <jerson.silva99@hotmail.com>
2023-02-10 19:21:34 -0500

    12. Introducción funciones function declaration

---
+++ Funciones/function-declaration.js
@@ -0,0 +1,10 @@
+// Funciones - Function declaration
+
+// Puedes llamar una función arriba o abajo de la función creada
+
+
+function sumar() { // Declarar una función
+    return 2 + 2 // Escribir código
+  }
+  
+sumar() // Lamar función
