9ec017ea166073f79a6539844c3c182154d8fa2e
devsilva <jerson.silva99@hotmail.com>
2023-02-17 16:47:44 -0500

    13. Introducción parametros y parametros por default

---
+++ Funciones/parametros-y-parametros-por-default.js
@@ -0,0 +1,14 @@
+// Funciones - Function declaration
+
+// Puedes llamar una función arriba o abajo de la función creada
+
+
+function sumar(numero1, numero2){
+  console.log(numero1 + numero2)
+}
+
+sumar(12, 23) // Asignar valor al llamar la función
+sumar(2, 3) // Siempre hay que pasar la cantidad de valores
+sumar(100, 400)
+sumar(12) // NaN al solo asignar un valor cuando se requieren 2 valores
+sumar() // NaN
