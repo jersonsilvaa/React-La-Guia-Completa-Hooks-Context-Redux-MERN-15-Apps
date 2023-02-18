8493ce6ec15a054c98c531ee0c6abd2b9220792a
devsilva <jerson.silva99@hotmail.com>
2023-02-17 20:07:51 -0500

    15 Introducción arrow functions

---
+++ Funciones/arrow-functions.js
@@ -0,0 +1,9 @@
+// Funciones - Arrow Functions
+
+// A diferencia de las funciones normales, un arrow function se declara de la siguiente manera.
+
+const sumar = (num1, num2) => num1 + num2
+
+sumar(10, 10)
+
+// Una arrow function se usa en pocos casos, pero siempre se usa para retornar algún valor en linea
