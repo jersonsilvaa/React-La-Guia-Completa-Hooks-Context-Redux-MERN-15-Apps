8c4926c4e0ccb106fc0fc9bab772b896d00c95d4
devsilva <jerson.silva99@hotmail.com>
2023-02-17 21:50:19 -0500

    18 Introducción condicionales if y operadores básicos

---
+++ Condicionales/operadores-y-condiciones-basicas.js
@@ -0,0 +1,23 @@
+// Condicionales if else else if
+
+// Disponible
+let num = 100000
+// Retirar
+let num2 = 20000
+
+// La cantidad disponible es mayor a la cantidad que se quiere retirar, por lo tanto se podrá hacer el retiro, en caso contrario la cantidad supere al disponible, no se podrá retirar
+if (num > num2) {
+    // Cuando se ejecuta la condición, se ejecuta este código
+    console.log('se puede retirar')
+} else {
+    // Cuando la condición no se cumple, se ejecuta este código
+    console.log('no se puede retirar')
+}
+
+// Operadores
+/**
+  > - Mayor que
+  < - Menor que
+  >= Mayor o igual que
+  <= Menor o igual que
+**/
